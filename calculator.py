aa317a77732d1b02b5b4a71a0c75df37ddcd1465
Carmo Da gama <greciomendis@gmail.com>
2021-12-21 21:28:41 +0100

    Gama

---
+++ calculator.py
@@ -0,0 +1,58 @@
+operators = '^*/+-'
+
+
+def get_list_expression(expression):
+    global operators
+    expression_list = []
+    member = ''
+    for character in expression:
+        if character in operators:
+            if member.isnumeric():
+                expression_list.append(member)
+                expression_list.append(character)
+            member = ''
+        else:
+            if character.isnumeric():
+                member += character
+
+    if member.isnumeric():
+        expression_list.append(member)
+
+    return expression_list
+
+
+def calc(number1, number2, operator):
+    if operator == '^':
+        return number1**number2
+    elif operator == '*':
+        return number1*number2
+    elif operator == '/':
+        return number1 / number2
+    elif operator == '-':
+        return number1 - number2
+    elif operator == '+':
+        return number1 + number2
+
+
+def calculate_expression(expression):
+    expressions = get_list_expression(expression)
+    for operator in operators:
+        count = len(expressions)-1
+        while operator in expressions:
+            if operator == expressions[count]:
+                number1 = float(expressions[count - 1])
+                number2 = float(expressions[count + 1])
+                result = calc(number1, number2, operator)
+                expressions.pop(count - 1)
+                expressions.pop(count)
+                expressions.pop(count + 1)
+                expressions.insert(0, result)
+                count -= 2
+
+
+
+
+expression = str(input('Type it Expression: '))
+
+print(get_list_expression(expression))
+print(f'Rseult: {calculate_expression(expression)}')
